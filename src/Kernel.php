2877d9579dd45f8f8e4058bf0da2e8393d10a758
ASBAITI Walid <walid.asbaiti@gmail.com>
2018-04-23 10:30:14 +0200

    Update Kernel.php
    Load conf by env after global routing configuration

--- src/Kernel.php
+++ src/Kernel.php
@@ -58,12 +58,12 @@ class Kernel extends BaseKernel
     protected function configureRoutes(RouteCollectionBuilder $routes)
     {
         $confDir = $this->getProjectDir().'/config';
+        $routes->import($confDir.'/routes'.self::CONFIG_EXTS, '/', 'glob');
         if (is_dir($confDir.'/routes/')) {
             $routes->import($confDir.'/routes/*'.self::CONFIG_EXTS, '/', 'glob');
         }
         if (is_dir($confDir.'/routes/'.$this->environment)) {
             $routes->import($confDir.'/routes/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
         }
-        $routes->import($confDir.'/routes'.self::CONFIG_EXTS, '/', 'glob');
     }
 }
